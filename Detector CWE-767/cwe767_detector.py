62ffa9f07906b9edc4e03f0d68529fac86cb5117
vaishalivanjari <vanjari.vaishali@gmail.com>
2023-04-03 07:44:08 -0500

    Update

--- Detector CWE-767/cwe767_detector.py
+++ Detector CWE-767/cwe767_detector.py
@@ -4,14 +4,14 @@ from slither.core.solidity_types.elementary_type import ElementaryType
 
 class CWE767detector(AbstractDetector):
     ARGUMENT = "CWE767Detector"  # slither will launch the detector with slither.py --mydetector
-    HELP = "Function named CWE767"
+    HELP = "CWE767:Critical private access in public method"
     IMPACT = DetectorClassification.HIGH
     CONFIDENCE = DetectorClassification.HIGH
     WIKI = "https://cwe.mitre.org/data/definitions/767.html"
-    WIKI_TITLE = "CWE767 example"
+    WIKI_TITLE = "CWE767 Vulnerability example"
     WIKI_DESCRIPTION = "Detector example"
-    WIKI_EXPLOIT_SCENARIO = ".."
-    WIKI_RECOMMENDATION = ".."
+    WIKI_EXPLOIT_SCENARIO = "Adding a detector to find unencrypted private data on-chain is the main goal of the project plan. Often held misconception: Private data variables cannot be read. Although the contract is not disclosed, attackers can learn about its status by looking at contract transactions."
+    WIKI_RECOMMENDATION = "To store private data on-chain or off-chain, it must be encrypted. Alternatively, private data can be modified in pure private function and that private function can be called in public function."
 
     def _detect(self):
         results = []
